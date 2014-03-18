00f9efb386482ac399b44e3010d1b3243eb1ab3d
JC de Groot <kris.de.groot@hva.nl>
2014-03-18 16:19:28 +0100

    Update

--- src/com/example/ijsbeacons/StartupActivity.java
+++ src/com/example/ijsbeacons/StartupActivity.java
@@ -3,16 +3,22 @@ package com.example.ijsbeacons;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.os.Build;
 
 public class StartupActivity extends Activity
 {
+	
+	Button button;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -24,6 +30,13 @@ public class StartupActivity extends Activity
 		{
 			getFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
+		final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
 	}
 
 	@Override
